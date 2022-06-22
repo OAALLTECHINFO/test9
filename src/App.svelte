7dc95fc40426b092ae5c7ba5d68733511b136021
OAALLTECHINFO <102165261+OAALLTECHINFO@users.noreply.github.com>
2022-06-22 14:29:33 +0400

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<style>
+	@import url("https://unpkg.com/fluent-svelte/theme.css");
+	:global(body) {
+		background-color: var(--fds-solid-background-base);
+		color: var(--fds-text-primary);
+	}
+</style>
+
+<script>
+	import { Router, Route, Link } from "svelte-navigator";
+	import { TextBox , Button } from "fluent-svelte";
+	import GradientText from 'svelte-gradient-typography';
+	let value = "";
+	import Blog from "./Blog.svelte";
+</script>
+
+<TextBox placeholder="Chose a Name" bind:value />
+
+<Router>
+    <nav>
+			<Link to="blog">Blog</Link>
+    </nav>
+
+  <main>
+    <Route path="/blog" component={Blog} />
+  </main>
+</Router>
+
+<Button href="/blog"/>
+
+<GradientText>
+    The {value}
+</GradientText>
---
+++ src/Blog.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { Router, Route, Link } from "svelte-navigator";
+  export let blogRoute;
+</script>
+
+<div class="Blog">
+  <h2>Blog</h2>
+  <span>You're at blog</span>
+  <nav>
+    <Link to="../">Go back to the landing page</Link>
+  </nav>
+</div>
